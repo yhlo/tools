19ececbbb62f179a182a1e41473cc543a9a171f6
Lo <tomangelia0928@gmail.com>
2023-10-17 17:10:50 +0800

    add temperature degree C to F

---
+++ tempC2F.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 16:19:31 2023
+
+@author: P170
+"""
+
+print("input the temperature in degree celsius (°C)")
+degreeC = float(input())
+
+degreeF = (9/5)*degreeC + 32
+print("degree Celsius (°C) is " + str(degreeC))
+print("degree Fahrenheit (°F) is " + str(degreeF))
+
